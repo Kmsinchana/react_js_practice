034a08401cda424437ddd3adc54382b6cb08b207
Kmsinchana <86287168+Kmsinchana@users.noreply.github.com>
2025-11-04 19:54:02 +0530

    commiting for first react app

---
+++ first_project_with_vite/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1> hello react app </h1>
+  )
+}
+export default App
+// always remember that the file has to start with capital letter and with .jsx otherwise there will be an error
