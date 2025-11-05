53af39bc1505a7faa8019b3997664478a9a725bc
Kmsinchana <86287168+Kmsinchana@users.noreply.github.com>
2025-11-05 16:18:29 +0530

    learned about the innerworking

--- first_project_with_vite/src/App.jsx
+++ first_project_with_vite/src/App.jsx
@@ -1,7 +1,18 @@
 
 function App() {
+  let username = "sinchana"
   return (
-    <h1> hello react app </h1>
+    <h1> hello react app {username} </h1>
+    // {username} is called a JSX Expression (or JSX Expression Placeholder).
+    //IMPORTANT
+    //we can't write anything inside the jsx statement like {if(username)} not valid
+    // because in in createElement it only expect the value not the statement
+    // let createReactElement = React.createElement (
+    //   "h1",
+    //   'hello react app'  //this is for the child 
+     //  if(username) // this will be invalid beause its against the standard jsx rule it should be always the value
+    // )
+    //we can write expression {username.toUpperCase()}
   )
 }
 export default App
